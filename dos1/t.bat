d9107dc0e5746834388ec6518c492f0795f65649
Jim Hall <jhall@freedos.org>
2022-01-07 17:44:26 -0600

    Add files via upload
    Sample files to test with the demo programs

---
+++ dos1/t.bat
@@ -0,0 +1,19 @@
+ech Hello world
+typ test.txt
+cpy ech.exe ech2.exe
+dir ech.exe ech2.exe
+ech2.exe This is a copy
+cpy file1
+cpy file1 file2
+cpy test.txt Y:\NODRIVE\file.txt
+cpy test.txt C:\NODIR\file.txt
+choic
+choic Yes or no?
+paus
+paus Press a key:
+paus Press a key for the next test:
+type test.txt | mor
+paus Press a key for the next test:
+type testlong.txt | mor
+paus Press a key for the next test:
+type test30.txt | mor
